c60faf4047d4cd709d59369e0b4f20563be88811
Nour Albarazi <nour.albarazi@friendventure.de>
2024-11-09 21:10:08 +0100

    fix build process to include another html files to compiled files in dist directory

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   server: {
     port: 3001,
   },
   base: "/berichtsheft-coded-prototype-avengers-ii/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        azubi: resolve(__dirname, "azubi.html"),
+        ihk: resolve(__dirname, "ihk.html"),
+      },
+    },
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
